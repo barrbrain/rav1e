cc1a7b37f7dd82cd3863a83e9f43da9c147bdd6d
David Michael Barr <b@rr-dav.id.au>
2018-02-08 23:26:30 +0900

    Revert "Show Existing Frames"
    This reverts commit becced10930ca10427507fa60048d9af4d53ab52.

--- src/bin/rav1e.rs
+++ src/bin/rav1e.rs
@@ -21,7 +21,6 @@ fn main() {
             break;
         }
         fi.number += 1;
-        fi.show_existing_frame = fi.number % 2 == 1;
         if fi.number == files.limit {
             break;
         }
