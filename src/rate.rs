0bfb18a4db01e81c2d979ae079f53e82e9456e4c
David Michael Barr <b@rr-dav.id.au>
2019-08-28 19:32:50 +0900

    Test no bias

--- src/rate.rs
+++ src/rate.rs
@@ -599,7 +599,7 @@ impl QuantizerParameters {
         select_ac_qi(quantizer_v, bit_depth).max(1),
       ],
       lambda,
-      dist_scale: [1.0, lambda / lambda_u, lambda / lambda_v],
+      dist_scale: [1.0, 1.0, 1.0],
     }
   }
 }
