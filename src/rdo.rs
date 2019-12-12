87b29b3fca1fc74cd59d0090b95d22684628b267
David Michael Barr <b@rr-dav.id.au>
2019-12-12 11:32:35 +0900

    Collect for regression

--- src/rdo.rs
+++ src/rdo.rs
@@ -139,6 +139,8 @@ pub fn estimate_rate(qindex: u8, ts: TxSize, fast_distortion: u64) -> u64 {
 fn cdef_dist_wxh_8x8<T: Pixel>(
   src1: &PlaneRegion<'_, T>, src2: &PlaneRegion<'_, T>, bit_depth: usize,
 ) -> RawDistortion {
+  use byteorder::{LittleEndian, WriteBytesExt};
+  use std::io::{self, Write};
   debug_assert!(src1.plane_cfg.xdec == 0);
   debug_assert!(src1.plane_cfg.ydec == 0);
   debug_assert!(src2.plane_cfg.xdec == 0);
@@ -148,6 +150,7 @@ fn cdef_dist_wxh_8x8<T: Pixel>(
 
   // Sum into columns to improve auto-vectorization
   let mut sum_s_cols: [u16; 8] = [0; 8];
+  let mut sum_d_cols: [u16; 8] = [0; 8];
   let mut sum_s2_cols: [u32; 8] = [0; 8];
   let mut sum_d2_cols: [u32; 8] = [0; 8];
   let mut sum_sd_cols: [u32; 8] = [0; 8];
@@ -155,8 +158,9 @@ fn cdef_dist_wxh_8x8<T: Pixel>(
   for j in 0..8 {
     let row1 = &src1[j][0..8];
     let row2 = &src2[j][0..8];
-    for (sum_s, sum_s2, sum_d2, sum_sd, s, d) in izip!(
+    for (sum_s, sum_d, sum_s2, sum_d2, sum_sd, s, d) in izip!(
       &mut sum_s_cols,
+      &mut sum_d_cols,
       &mut sum_s2_cols,
       &mut sum_d2_cols,
       &mut sum_sd_cols,
@@ -167,6 +171,7 @@ fn cdef_dist_wxh_8x8<T: Pixel>(
       let s: u16 = u16::cast_from(*s);
       let d: u16 = u16::cast_from(*d);
       *sum_s += s;
+      *sum_d += d;
 
       // Convert to u32 to avoid overflows when multiplying
       let s: u32 = s as u32;
@@ -181,19 +186,28 @@ fn cdef_dist_wxh_8x8<T: Pixel>(
   // Sum together the sum of columns
   let sum_s: i64 =
     sum_s_cols.iter().map(|&a| u32::cast_from(a)).sum::<u32>() as i64;
+  let sum_d: i64 =
+    sum_d_cols.iter().map(|&a| u32::cast_from(a)).sum::<u32>() as i64;
   let sum_s2: i64 = sum_s2_cols.iter().sum::<u32>() as i64;
   let sum_d2: i64 = sum_d2_cols.iter().sum::<u32>() as i64;
   let sum_sd: i64 = sum_sd_cols.iter().sum::<u32>() as i64;
 
   // Use sums to calculate distortion
   let svar = sum_s2 - ((sum_s * sum_s + 32) >> 6);
+  let dvar = sum_d2 - ((sum_d * sum_d + 32) >> 6);
   let sse = (sum_d2 + sum_s2 - 2 * sum_sd) as f64;
   // Temporary constants to match old distribution of scaling.
   let ssim_boost = (4033_f64 / 16_384_f64)
     * (svar + svar + (16_384 << (2 * coeff_shift))) as f64
-    / f64::sqrt(((16_265_089i64 << (4 * coeff_shift)) + svar * svar) as f64)
-    * 0.869_873_046_875f64
-    + 0.150_146_484_375f64;
+    / f64::sqrt(((16_265_089i64 << (4 * coeff_shift)) + svar * svar) as f64);
+  let ssim_boost_orig = (4033_f64 / 16_384_f64)
+    * (svar + dvar + (16_384 << (2 * coeff_shift))) as f64
+    / f64::sqrt(((16_265_089i64 << (4 * coeff_shift)) + svar * dvar) as f64);
+  let mut buf = ArrayVec::<[u8; 16]>::new();
+  buf.write_f64::<LittleEndian>(ssim_boost).unwrap();
+  buf.write_f64::<LittleEndian>(ssim_boost_orig).unwrap();
+  io::stdout().write_all(&buf);
+  let ssim_boost = ssim_boost * 0.869_873_046_875f64 + 0.150_146_484_375f64;
   RawDistortion::new((sse * ssim_boost + 0.5_f64) as u64)
 }
 
