8cf405b77cdccdae5a4a6493b0b616eed933c63a
David Michael Barr <b@rr-dav.id.au>
2020-08-05 00:13:59 +0900

    Disable TX domain distortion, enable temporal RDO with tune PSNR

--- src/api/config/speedsettings.rs
+++ src/api/config/speedsettings.rs
@@ -198,7 +198,7 @@ impl SpeedSettings {
 
   /// TX domain distortion is always faster, with no significant quality change
   const fn tx_domain_distortion_preset(_speed: usize) -> bool {
-    true
+    false
   }
 
   const fn tx_domain_rate_preset(_speed: usize) -> bool {
