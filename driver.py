d7b2841874a433967609f21b6cfb4e3a0774fe1c
EC2 Default User <ec2-user@ip-172-31-30-204.ap-northeast-2.compute.internal>
2023-06-08 02:56:43 +0000

    Data collection reconfiguration

--- driver.py
+++ driver.py
@@ -5,15 +5,12 @@ import subprocess
 import sys
 
 def neg_avg_psnr(strength, filename, q):
-  cmd = "target/debug/rav1e -o /dev/null -s8 --tiles 4 -v --tune Psnr --psnr --strength".split() + \
-    [str(*strength), filename, '--quantizer', str(q)]
-  print(' '.join(cmd))
-  if strength[0] < 1.0: return 0
-  if strength[0] > 3.0: return 0
-  with subprocess.Popen(cmd, stderr=subprocess.PIPE) as proc:
+  cmd = f"zstd -d < {filename} | target/release/rav1e -o /dev/null -s5 --threads 1 --tiles 1 -v --psnr --no-scene-detection --keyint 0 --strength {strength[0]} - --quantizer {q}"
+  print(cmd)
+  with subprocess.Popen(cmd, stderr=subprocess.PIPE, shell=True) as proc:
     for line in proc.stderr:
       # >  encoded 60 frames, 6.064 fps, 37.86 Kb/s, elapsed: 9s
-      if b'encoded 60 frames' in line and b'Input Frame' not in line:
+      if b'encoded' in line and b'Input Frame' not in line:
         print(line.decode().rstrip())
         rate = float(line.split()[7].decode())
       # >  Mean PSNR: Avg: 41.4705  Y: 40.2983  Cb: 45.6217  Cr: 45.7722
@@ -24,9 +21,10 @@ def neg_avg_psnr(strength, filename, q):
   return 0
 
 with open(sys.argv[1] + ".csv", "w") as f:
-  for q in [252, 220, 172, 128, 80]:
+  # 78 98 118 138 158 188
+  for q in [188, 158, 138, 118, 98, 78]:
     args = (sys.argv[1], q)
-    full_scan = np.linspace(1.0, 3.0, 21)
+    full_scan = np.linspace(1.5, 4.5, 31)
     for x in full_scan:
       v = neg_avg_psnr([x], *args)
       f.write(",".join(map(str, v)) + "\n")
